a5dba030a905149661eab662dab9950f55ea58da
ManciuAna <manciu_anamaria@yahoo.ro>
2024-12-21 14:08:28 +0000

    testing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+
 import './partials/header/header';
 import './partials/header/menu';
+
+import { popularRecipesRendering } from './components/PopularRecipes/PopularRecipes.js';
+import './components/Header/Header.js'
+import './components/Header/header-menu.js'
+import './components/Categories/Categories.js'
+import './components/Hero/swiper.js'
+import './components/OrderNow/OrderNow.js'
+import './components/ModalRecipe/ModalRecipe.js'
+import './components/scroll-to-top/scrollTop.js'
+import './components/Rating/Rating.js'
+
