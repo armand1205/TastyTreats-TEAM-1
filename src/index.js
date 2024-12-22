528d640ccda9e47246f474593d663e9ec9e47045
SimpleRoDev <146526864+SimpleRoDev@users.noreply.github.com>
2024-12-23 00:02:25 +0200

    Last Update
    All code was reviewed and modify for updates to be working.
    This is the final update with all files working correctly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
+import './js/mobMenu';
 
-import './partials/header/header';
-import './partials/header/menu';
+import './js/changeTheme';
 
-import { popularRecipesRendering } from './components/PopularRecipes/PopularRecipes.js';
-import './components/Header/Header.js'
-import './components/Header/header-menu.js'
-import './components/Categories/Categories.js'
-import './components/Hero/swiper.js'
-import './components/OrderNow/OrderNow.js'
-import './components/ModalRecipe/ModalRecipe.js'
-import './components/scroll-to-top/scrollTop.js'
-import './components/Rating/Rating.js'
+import './js/index/hero';
 
+import './js/index/search.js';
+
+import './js/index/fill_categories';
+
+import './js/API_request/defaultRequest';
+
+import './js/index/allCategories';
+
+import './js/index/popular';
+
+import './js/index/hero_modal';
+
+import './js/index/area';
+
+import './js/index/ingredients';
+
+import './js/index/time';
+
+import './js/index/filterRender';
+
+import './js/pagination';
+
+import './js/changeTheme';
+
+import './js/modalRecipe';
+
+import './js/modalRating';
+
+import './js/headerPageAftreSwitch';
+
+import './js/scrollToTop';
+
+import './js/API_request/rating-modal';
+
+import './js/TeamModalOpCl';
+
+import './js/utils/validation';
